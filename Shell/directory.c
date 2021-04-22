5cbd26c7d51dd6663faa9f297eb4f1f1bc2274ad
Miryala Narayana Reddy <narayana@git.com>
2021-04-22 18:51:08 +0530

    added directory struct

--- Shell/directory.c
+++ Shell/directory.c
@@ -1,15 +1,34 @@
 #include "directory.h"
 
-void InitializePath()
+// void InitializePath()
+// {
+//     strcpy(PATH_OF_CURRENT_DIRECTORY, "../user");
+// }
+
+// void Print_current_directory_path()
+// {
+//     int i = 3;
+//     while (PATH_OF_CURRENT_DIRECTORY[i] != '\0')
+//     {
+//         printf("%c", PATH_OF_CURRENT_DIRECTORY[i++]);
+//     }
+// }
+
+PtrToDirec NewDirec()
 {
-    strcpy(PATH_OF_CURRENT_DIRECTORY, "../user");
+    PtrToDirec new_direc = (PtrToDirec)malloc(sizeof(Direc));
+    return new_direc;
 }
 
-void Print_current_directory_path()
+
+PtrToDirec InitializeDirec()
 {
-    int i = 3;
-    while (PATH_OF_CURRENT_DIRECTORY[i] != '\0')
-    {
-        printf("%c", PATH_OF_CURRENT_DIRECTORY[i++]);
-    }
+    DIR *dir = opendir("../");
+    struct dirent* curr_dir = readdir(dir);
+    PtrToDirec DirecTree = NewDirec();
+    strcpy(DirecTree.Name,curr_dir->d_name);
+    
+    
+    
+    close(dir);
 }
--- Shell/directory.h
+++ Shell/directory.h
@@ -4,12 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#define MAX_LEN_NAME 21
 
-#define MAX_PATH_LEN 1000
+typedef Directory Direc;
+typedef Directory PtrToDirec;
 
-char PATH_OF_CURRENT_DIRECTORY[MAX_PATH_LEN];
+struct Directory
+{
+    char Name[MAX_LEN_NAME];
+    Direc *PtrToSubDirecs;
+    Direc *PtrToSubFiles;
+    Direc *Next;
+};
 
-void InitializePath();
-void Print_current_directory_path();
+PtrToDirec InitializeDirec();
+
+// define MAX_PATH_LEN 1000
+
+// char PATH_OF_CURRENT_DIRECTORY[MAX_PATH_LEN];
+
+// void InitializePath();
+// void Print_current_directory_path();
 
 #endif
