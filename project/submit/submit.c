9faf5f8039e4bf71b69af4efdce8f359db528a3c
poorvapisal <82924157+poorvapisal@users.noreply.github.com>
2021-04-26 12:50:53 +0530

    Create submit.c

---
+++ project/submit/submit.c
@@ -0,0 +1,27 @@
+#include "submit.h"
+
+void execute(PtrToDirec name, char *input_string)
+{
+    char exe_command[MAX_PATH_LEN];
+    strcpy(exe_command, "zip -r ");
+    strcat(exe_command, name->Path);
+    strcat(exe_command, ".zip ");
+    strcat(exe_command, name->Path);
+
+    printf("%s\n", exe_command);
+    system(exe_command);
+}
+
+void zipper(char *input_string)
+{
+    char folder[1000];
+    char final_zip[1000];
+
+    PtrToDirec ptrtofolder = FindDirectory(CURRENT_DIRECTORY, input_string);
+
+    if (ptrtofolder == NULL)
+        printf("File does not exist\n");
+
+    else
+        execute(ptrtofolder, input_string);
+}
