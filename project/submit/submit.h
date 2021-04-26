4c363365b70b716f5aedde34b31c0d5dbac17037
poorvapisal <82924157+poorvapisal@users.noreply.github.com>
2021-04-26 12:51:39 +0530

    Create submit.h

---
+++ project/submit/submit.h
@@ -0,0 +1,19 @@
+#ifndef __SUBMIT_H
+#define __SUBMIT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <dirent.h>
+
+#include "../Shell/user.h"
+#include "../switch/switch.h"
+#include "../Shell/directory.h"
+
+void zipper(char *input_string);
+void execute(PtrToDirec name, char *input_string);
+
+#endif
