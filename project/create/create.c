b2cb44c72c85e370419ee882654eca4acc1a831d
mudit-source <82899177+mudit-source@users.noreply.github.com>
2021-05-04 20:24:14 +0530

    fixed tree not updating after copying

--- project/create/create.c
+++ project/create/create.c
@@ -23,28 +23,34 @@ void copy_everything(char *destination)
 
 void create(char *input_string)
 {
-    MakeNewFolder(input_string);
-    copy_everything(input_string);
-   // InitializeDirecTree(input_string, CURRENT_DIRECTORY->Path);
-}
-
-void MakeNewFolder(char *input_string)
-{
     char new_assign[MAX_PATH_LEN];
     strcpy(new_assign, PATH_OF_CURRENT_DIRECTORY);
     strcat(new_assign, "/");
     strcat(new_assign, input_string);
-    mkdir(new_assign, 0700);
-    DIR *curr_dir = opendir(PATH_OF_CURRENT_DIRECTORY);
+    PtrToDirec dir = FindDirectory(CURRENT_DIRECTORY, input_string);
+
+    if (dir == NULL)
+    {
+        MakeNewFolder(new_assign);
+    }
+
+    copy_everything(input_string);
+    
+    //this is for updating the tree in the code
+    PtrToDirec New_dir = InitializeDirecTree(input_string,new_assign);
+    New_dir->Next = CURRENT_DIRECTORY->PtrToSubDirecs;
+    CURRENT_DIRECTORY->PtrToSubDirecs = New_dir;
+}
+
+void MakeNewFolder(char *Path_to_new_directory)
+{
+
+    mkdir(Path_to_new_directory, 0700);
+    DIR *curr_dir = opendir(Path_to_new_directory);
 
     if (curr_dir)
     {
         closedir(curr_dir);
-        //this is for updating the tree in the code
-        PtrToDirec New_dir = NewDirec(input_string);
-        New_dir->Next = CURRENT_DIRECTORY->PtrToSubDirecs;
-        CURRENT_DIRECTORY->PtrToSubDirecs = New_dir;
-        strcpy(CURRENT_DIRECTORY->PtrToSubDirecs->Path, new_assign);
     }
     else
     {
