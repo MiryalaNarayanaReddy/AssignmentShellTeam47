d741a6421a11edb83d9bb7a07dbc77a8e84f5df7
Tishadubey01 <76221139+Tishadubey01@users.noreply.github.com>
2021-05-09 18:18:15 +0530

    Update update.c

--- project/update/update.c
+++ project/update/update.c
@@ -11,19 +11,20 @@
 #include <unistd.h>
 #include <assert.h>
 #include "../create/create.h"
+#include "../Shell/user.h"
 
-void copy_file(char *source, char *destination)//copies a file from source to destination
+void copy_file(char *source, char *destination) //copies a file from source to destination
 {
     char exe_command[MAX_PATH_LEN];
     char s[MAX_PATH_LEN];
     char d[MAX_PATH_LEN];
 
     strcpy(exe_command, "cp ");
-//strcpy- It is a C library function which copies 2nd string to the 1st
+    //strcpy- It is a C library function which copies 2nd string to the 1st
     strcpy(s, source);
     AddSlashSpaceInSubjectName(s); /////// source
     strcat(exe_command, s);
-//strcat- It is a C library function include in string.h header file which concatenates the last string and the first string and stores the result in first string.
+    //strcat- It is a C library function include in string.h header file which concatenates the last string and the first string and stores the result in first string.
     strcat(exe_command, " ");
 
     strcpy(d, destination);
@@ -34,7 +35,7 @@ void copy_file(char *source, char *destination)//copies a file from source to de
     //Using system(), we can execute any command that can run on terminal if operating system allows.
 }
 
-void remove_file(PtrToDirec file)//to remove the file which has been passed in the function.
+void remove_file(PtrToDirec file) //to remove the file which has been passed in the function.
 {
     char clean_it[MAX_PATH_LEN];
     char path[MAX_PATH_LEN];
@@ -52,11 +53,11 @@ char *get_last_modified_date(PtrToDirec file)
     char *time = (char *)malloc(sizeof(char) * MAX_LEN_NAME);
     char path[MAX_PATH_LEN];
     strcpy(time, "stat -c '%y' ");
-    strcpy(path,file->Path);
+    strcpy(path, file->Path);
     AddSlashSpaceInSubjectName(path);
-    strcat(time,path);
+    strcat(time, path);
 
-    FILE *fp = fopen("i.txt", "w");//(fopen() open the file pointed to using the given mode)
+    FILE *fp = fopen("i.txt", "w"); //(fopen() open the file pointed to using the given mode)
     //"w"=Creates an empty file for writing. If already exist then the contents are erased and is considered an empty file.
     fclose(fp);
 
@@ -66,7 +67,7 @@ char *get_last_modified_date(PtrToDirec file)
     fp = fopen("i.txt", "r");
     //"r"= Opens an existed file for reading.
     int i = 0;
-    while (!feof(fp))//while end of the file is not visited i.e all the characters of i.txt get to read, the while loop continues.
+    while (!feof(fp)) //while end of the file is not visited i.e all the characters of i.txt get to read, the while loop continues.
     {
         fscanf(fp, "%ch", &time[i]);
         i++;
@@ -108,15 +109,17 @@ void check_for_updates(PtrToDirec root, PtrToDirec check_folder)
         {
             last_md_of_assign = get_last_modified_date(temp);
             last_md_of_copy = get_last_modified_date(curr);
-            if (strcmp(last_md_of_assign, last_md_of_copy) > 0)//checks for the modification time of existed file and changed one(copied)
+            if (strcmp(last_md_of_assign, last_md_of_copy) > 0)
             {
                 remove_file(curr);
                 copy_file(temp->Path, curr->Path);
+
+                print_in_color(temp->Name, PINK, NOTBOLD);
+                printf(" is updated\n");
             }
             free(last_md_of_assign);
             free(last_md_of_copy);
         }
-        //If a new file has been added
         else
         {
             PtrToDirec new_file = NewDirec(temp->Name);
@@ -128,6 +131,8 @@ void check_for_updates(PtrToDirec root, PtrToDirec check_folder)
             strcat(new_file->Path, temp->Name);
 
             copy_file(temp->Path, new_file->Path);
+            print_in_color(temp->Name, GREEN, NOTBOLD);
+            printf(" is new file\n");
         }
         temp = temp->Next;
     }
@@ -135,7 +140,14 @@ void check_for_updates(PtrToDirec root, PtrToDirec check_folder)
 
 void update(char *input_string)
 {
+    char error_str[MAX_LEN_NAME];
     PtrToDirec check_folder = FindDirectory(CURRENT_DIRECTORY, input_string);
     PtrToDirec assign_folder = InitializeDirecTree("assignment", "../../assignment");
+    if (check_folder == NULL)
+    {
+        sprintf(error_str, "%s - No such folder\n", input_string);
+        print_in_color(error_str, RED, NOTBOLD);
+        return;
+    }
     check_for_updates(assign_folder, check_folder);
 }
