ef4a9b3250545ca3b510d6115d9954fcc87c3f10
MiryalaNarayanaReddy <81623820+MiryalaNarayanaReddy@users.noreply.github.com>
2021-05-09 18:55:46 +0530

    Add files via upload

---
+++ test case/dist/submitter.py
@@ -0,0 +1,8 @@
+print("hello world")
+print("\nERROR: you are printing 10 squares \n")
+print("I want only 9 donot include \"0 x 0 = 0\"\n")
+i = 0
+while i < 10:
+    string = str(i) + " x " + str(i) + " = " + str(i*i)
+    print(string)
+    i = i+1
